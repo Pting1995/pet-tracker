10f79e0334a67fa1b0d2af22b7a578e7c0dc66c3
Peter Ting <pting1995@gmail.com>
2021-02-02 15:46:38 -0800

    added if else statment to connection

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,22 @@
 var mysql = require("mysql");
 
 // for local
+if (process.env.JAWSDB_URL) {
+  // for Heroku
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "food_db"
+  });
+}
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "password",
-//   database: "food_db"
-// });
 
-// for Heroku
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
 
 connection.connect(function (err) {
   if (err) {
